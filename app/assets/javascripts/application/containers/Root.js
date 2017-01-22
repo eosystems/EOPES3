72e97f9a6179f4c9704cb03d9654867b80e063a7
r_nishio <quick.xp@me.com>
2017-01-22 20:31:55 +0900

    redux-router success

--- app/assets/javascripts/application/containers/Root.js
+++ app/assets/javascripts/application/containers/Root.js
@@ -5,9 +5,18 @@ import estimateIndex from './estimate/estimateIndex';
 import configureStore from '../store/configureStore';
 import { Router, Route, browserHistory } from 'react-router';
 import { syncHistroyWithStore } from 'react-router-redux';
+import { createStore, combineReducers } from 'redux'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
-const store = configureStore();
-const history = syncHistroyWithStore(browserHistory, store);
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    routing: routerReducer
+  })
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
 
 export default class Root extends Component {
   render() {
