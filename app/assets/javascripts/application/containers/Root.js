15c263150084e7623c0f8aad6c8d09efb8a788de
r_nishio <quick.xp@me.com>
2017-01-23 22:10:27 +0900

    root reducer success

--- app/assets/javascripts/application/containers/Root.js
+++ app/assets/javascripts/application/containers/Root.js
@@ -4,12 +4,9 @@ import { Provider } from 'react-redux';
 import estimateIndex from './estimate/estimateIndex';
 import configureStore from '../store/configureStore';
 import { Router, Route, browserHistory } from 'react-router';
-import { createStore, combineReducers } from 'redux'
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
-
-// Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store)
 
 export default class Root extends Component {
--- app/assets/javascripts/application/store/configureStore.js
+++ app/assets/javascripts/application/store/configureStore.js
@@ -1,20 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { rootReducer } from '../reducers';
-import { routerReducer } from 'react-router-redux'
+import rootReducer from '../reducers';
+import { routerReducer } from 'react-router-redux';
 
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore);
 
-//export default function configureStore(initialState) {
-//  return createStoreWithMiddleware(rootReducer, initialState);
-//}
-
 export default function configureStore(initialState) {
-  return  createStore(
-    combineReducers({
-    routing: routerReducer
-    })
-  )
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
