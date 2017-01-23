2845e9d6ee3b53afa5c5b8e5511930f500eea005
r_nishio <quick.xp@me.com>
2017-01-23 21:44:51 +0900

    configureStore

--- app/assets/javascripts/application/containers/Root.js
+++ app/assets/javascripts/application/containers/Root.js
@@ -4,16 +4,10 @@ import { Provider } from 'react-redux';
 import estimateIndex from './estimate/estimateIndex';
 import configureStore from '../store/configureStore';
 import { Router, Route, browserHistory } from 'react-router';
-import { syncHistroyWithStore } from 'react-router-redux';
 import { createStore, combineReducers } from 'redux'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import { syncHistoryWithStore } from 'react-router-redux';
 
-// Add the reducer to your store on the `routing` key
-const store = createStore(
-  combineReducers({
-    routing: routerReducer
-  })
-)
+const store = configureStore();
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store)
--- app/assets/javascripts/application/store/configureStore.js
+++ app/assets/javascripts/application/store/configureStore.js
@@ -1,11 +1,20 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from '../reducers';
+import { routerReducer } from 'react-router-redux'
 
 const createStoreWithMiddleware = applyMiddleware(
   thunk
 )(createStore);
 
+//export default function configureStore(initialState) {
+//  return createStoreWithMiddleware(rootReducer, initialState);
+//}
+
 export default function configureStore(initialState) {
-  return createStoreWithMiddleware(rootReducer, initialState);
+  return  createStore(
+    combineReducers({
+    routing: routerReducer
+    })
+  )
 }
