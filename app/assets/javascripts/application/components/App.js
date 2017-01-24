c6fc9fe4bdeef41a03e70036cb0402af013f8d8f
r_nishio <quick.xp@me.com>
2017-01-25 00:09:37 +0900

    Product Estimate画面 レイアウト

--- app/assets/javascripts/application/components/App.js
+++ app/assets/javascripts/application/components/App.js
@@ -1,16 +1,21 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {RaisedButton} from 'material-ui';
+import AppBar from 'material-ui/AppBar';
+
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.children}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme()}>
+                <div>
+                    <AppBar title="EOPES" iconClassNameRight="muidocs-icon-navigation-expand-more" onTouchTap={this.handleRequestClose}/>
+                      {this.props.children}
+                </div>
+            </MuiThemeProvider>
+        )
+    }
 }
 
-
-App.propTypes = {
-
-};
+App.propTypes = {};
 
 export default App;
--- app/assets/javascripts/application/components/estimate/EstimateProductEdit.js
+++ app/assets/javascripts/application/components/estimate/EstimateProductEdit.js
@@ -1,18 +1,30 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 class EstimateProductEdit extends Component {
-  render() {
-    const { increment, incrementIfOdd, incrementAsync, decrement, counter } = this.props;
-    return (
-      <p>
-        materials
-      </p>
-    );
-  }
+    render() {
+        const {increment, incrementIfOdd, incrementAsync, decrement, counter} = this.props;
+        return (
+            <div>
+                <div className="title">
+                    Product Estimate
+                </div>
+                <div className="estimate-summary">
+                  Profit 100,000 isk (82.2%)
+                </div>
+                <Tabs>
+                  <Tab label="Blueprint">
+                    <p>blueprint select</p>
+                  </Tab>
+                  <Tab label="Material">
+                    <p>material select</p>
+                  </Tab>
+                </Tabs>
+            </div>
+        );
+    }
 }
 
-EstimateProductEdit.propTypes = {
-
-};
+EstimateProductEdit.propTypes = {};
 
 export default EstimateProductEdit;
