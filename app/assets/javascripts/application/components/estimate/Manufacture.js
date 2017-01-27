09754fb351464503cd38b6ea124ef1b63ade7c2f
r_nishio <quick.xp@me.com>
2017-01-28 00:43:06 +0900

    text value change

--- app/assets/javascripts/application/components/estimate/Manufacture.js
+++ app/assets/javascripts/application/components/estimate/Manufacture.js
@@ -23,21 +23,21 @@ class Manufacture extends React.Component {
         return (
             <div>
                 <div className="sub-title">Blueprint</div><br/>
-                <Select name="blueprint-dropdown" value={blueprint.bpc} options={blueprintOptions} onChange={blueprintChange}/>
-                <TextField name="te" value={blueprint.te} floatingLabelText="TE"/>
-                <TextField name="me" value={blueprint.me} floatingLabelText="ME"/>
-                <TextField name="runs" value={blueprint.runs} floatingLabelText="RUNS"/>
+                <Select name="blueprint-dropdown" defaultValue={blueprint.bpc} options={blueprintOptions} onChange={blueprintChange}/>
+                <TextField name="te" defaultValue={blueprint.te} floatingLabelText="TE"/>
+                <TextField name="me" defaultValue={blueprint.me} floatingLabelText="ME"/>
+                <TextField name="runs" defaultValue={blueprint.runs} floatingLabelText="RUNS"/>
                 <br/>
 
                 <div className="sub-title">Character</div><br/>
-                <TextField name="industry" value={character.industry} floatingLabelText="Industry Skill"/>
-                <TextField name="advancedIndustry" value={character.advancedIndustry} floatingLabelText="Advanced Industry Skill"/>
+                <TextField name="industry" defaultValue={character.industry} floatingLabelText="Industry Skill"/>
+                <TextField name="advancedIndustry" defaultValue={character.advancedIndustry} floatingLabelText="Advanced Industry Skill"/>
                 <br/>
 
                 <div className="sub-title">Installation</div><br/>
-                <TextField name="solarSystem" value={installation.solarSystem} floatingLabelText="Solar System"/>
-                <TextField name="manufacturing" value={installation.manufacturing} floatingLabelText="Manufacturing"/>
-                <TextField name="tax" value={installation.tax} floatingLabelText="Installation Tax"/>
+                <TextField name="solarSystem" defaultValue={installation.solarSystem} floatingLabelText="Solar System"/>
+                <TextField name="manufacturing" defaultValue={installation.manufacturing} floatingLabelText="Manufacturing"/>
+                <TextField name="tax" defaultValue={installation.tax} floatingLabelText="Installation Tax"/>
 
             </div>
         );
