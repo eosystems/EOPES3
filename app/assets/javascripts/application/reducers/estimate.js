1284612dd7e9c18b4a03adcd79bbe5fd99f61028
r_nishio <quick.xp@me.com>
2017-01-28 00:36:11 +0900

    tab change success

--- app/assets/javascripts/application/reducers/estimate.js
+++ app/assets/javascripts/application/reducers/estimate.js
@@ -1,33 +1,38 @@
-import {TAB_CHANGE} from '../actions/estimateProductEdit';
+import {
+    TAB_CHANGE
+} from '../actions/estimateProductEdit';
 
-const initialState =
-{
-  blueprint: {
-    bpc: '',
-    te: 10,
-    me: 20,
-    runs: 1
-  },
-  character: {
-    industry: 5,
-    advancedIndustry: 5
-  },
-  installation: {
-    solarSystem: '',
-    manufacturing: 'Station',
-    tax: '10.0'
-  },
-  tabValue: 'manufacturing'
+const initialState = {
+    blueprint: {
+        bpc: '',
+        te: 10,
+        me: 20,
+        runs: 1
+    },
+    character: {
+        industry: 5,
+        advancedIndustry: 5
+    },
+    installation: {
+        solarSystem: '',
+        manufacturing: 'Station',
+        tax: '10.0'
+    },
+    tabValue: 'manufacturing'
 }
 
 export default function estimate(state = initialState, action) {
-  let price = state.price
-  switch (action.type) {
-    case 'ACTION_INCREMENT_PRICE':
-      return {price: price + action.price}
-    case 'TAB_CHANGE':
-      return action;
-  default:
-    return state;
-  }
+    let price = state.price
+    switch (action.type) {
+        case 'ACTION_INCREMENT_PRICE':
+            return {
+                price: price + action.price
+            }
+        case 'TAB_CHANGE':
+            return Object.assign({}, state,{
+              tabValue: action.tabChange
+            })
+        default:
+            return state;
+    }
 }
