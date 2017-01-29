1cd5fc7b71c11563b8a8247e2207bf46fd4dca34
r_nishio <quick.xp@me.com>
2017-01-29 20:38:13 +0900

    test

--- app/assets/javascripts/application/lib/EstimateLogic.js
+++ app/assets/javascripts/application/lib/EstimateLogic.js
@@ -1,10 +1,12 @@
 export default class EstimateLogic{
   requireMaterial(runs, baseQuantity, me){
     const facilityModifire = 1.0;
-    result = (runs * baseQuantity * facilityModifire * (1.0 - me * 0.01)).ceil
+    let result = (runs * baseQuantity * facilityModifire * (1.0 - me * 0.01))
     if (result < runs){
       return runs;
     }
+    console.log("test------");
+    console.log(result);
     return result;
   }
 }
--- spec/client/lib/EstimateLogicSpec.js
+++ spec/client/lib/EstimateLogicSpec.js
@@ -1,12 +1,15 @@
+import EstimateLogic from '../../../app/assets/javascripts/application/lib/EstimateLogic';
+
 var assert = require('assert');
-let EsimateLogic = require('../../../app/assets/javascripts/application/lib/EstimateLogic.js');
+//let EsimateLogic = require('../../../app/assets/javascripts/application/lib/EstimateLogic.js');
 
 describe('Estimate', function() {
   describe('requireMaterial', function() {
     it('require material logic', function() {
       let logic = new EstimateLogic();
-      let result = logic.requireMaterial(1,1,1)
-      assert.equal(-1, result);
+      let result = logic.requireMaterial(1,1,1);
+      console.log(result);
+      assert.equal(1, result);
     });
   });
 });
