93d4ab1552606b5015ff801c2dac1f80bb5d8ee2
r_nishio <quick.xp@me.com>
2017-01-29 16:45:01 +0900

    Redux Dev Tools

--- app/assets/javascripts/application/store/configureStore.js
+++ app/assets/javascripts/application/store/configureStore.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { routerReducer } from 'react-router-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
+const createStoreWithMiddleware = composeWithDevTools(
+  applyMiddleware(
+    thunk
+  )
 )(createStore);
 
 export default function configureStore(initialState) {
