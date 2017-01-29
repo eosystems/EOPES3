ba9f2985fd724cec828432282beb7bddbba5d853
r_nishio <quick.xp@me.com>
2017-01-29 14:56:59 +0900

    material tab

--- app/assets/javascripts/application/components/estimate/Material.js
+++ app/assets/javascripts/application/components/estimate/Material.js
@@ -1,17 +1,24 @@
 import React, {Component, PropTypes} from 'react';
 import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import ActionAssessment from 'material-ui/svg-icons/action/assessment';
+import {fullWhite} from 'material-ui/styles/colors';
 
 const style = {
-  height: 64,
-  textAlign: 'center',
-  display: 'inline-block',
-  margin: 20,
-  width: 150
+    height: 64,
+    textAlign: 'center',
+    display: 'inline-block',
+    margin: 20,
+    width: 36
 };
 
 const shortTextStyle = {
-  width: 150
+    width: 150
+}
+
+const marketButtonStyle = {
+    margin: 10
 }
 
 class Material extends React.Component {
@@ -27,18 +34,26 @@ class Material extends React.Component {
             orderType,
             orderSolarSystem
         } = this.props.data;
+
+        // inline text
         let imgSrc = "https://image.eveonline.com/Type/" + typeId + "_32.png";
+        let requiredMaterialText = typeName + " x " + require;
+        let orderTypeText = orderType + ": " + orderSolarSystem;
+
         return (
             <div>
 
                 <Paper style={style} zDepth={0}>
-                    <img src={imgSrc}></img><br/>
-                    {typeName} x {require}
+                    <img src={imgSrc}></img>
                 </Paper>
+                <span className="estimate-fixed-text">
+                    {requiredMaterialText}
+                </span>
                 <TextField style={shortTextStyle} name="volume" value={volume} floatingLabelText="Total Volume"/>
                 <TextField style={shortTextStyle} name="price" defaultValue={price} floatingLabelText="Unit Price"/>
                 <TextField name="totalPrice" value={totalPrice} floatingLabelText="Total Price"/>
-                <span className="estimate-text">{orderType} : {orderSolarSystem}</span>
+                <span className="estimate-text">{orderTypeText}</span>
+                <FlatButton label="Market" labelPosition="before" style={marketButtonStyle} icon={< ActionAssessment/>}/>
             </div>
         );
     }
